492ab5d94e88b68a05efa562e2c473cc31b8043e
YoungZ2357 <zqy2357@outlook.com>
2026-06-29 11:47:16 +0800

    chore: 新增图可视化脚本

---
+++ scripts/draw_graph.py
@@ -0,0 +1,14 @@
+import os
+
+from langgraph.graph.state import CompiledStateGraph
+
+
+
+def draw_graph(graph: CompiledStateGraph, save_directory: str = "", file_name: str = "graph.png") -> None:
+
+    png = graph.get_graph().draw_mermaid_png()
+
+    file_path = os.path.join(save_directory, file_name)
+    with open("docs/graph.png", "wb") as f:
+        f.write(png)
+
